34af065f02e691c712d0a30d58016b60639cab27
Qkauia <kauia96@gmail.com>
2023-11-30 23:07:54 +0800

    soft delete

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :price, numericality: { greate_than: 0 }
+
+  default_scope { where(deleted_at: nil) }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
